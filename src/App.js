6ea5b2ff7461eb62216d0e0ec03ae89b331d9fdf
Milisav2812 <milisaw95@gmail.com>
2021-09-26 09:50:55 +0200

    # Start of the Meetups project - Routes generated - Navigation set

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import {Route, Switch} from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
 
 
 function App() {
-  return <div> Hello! </div>;
+  return <div> 
+    <MainNavigation/>
+
+    <Switch>
+      <Route path="/" exact>
+        <AllMeetupsPage/>
+      </Route>
+
+      <Route path="/new-meetups">
+        <NewMeetupPage/>
+      </Route>
+
+      <Route path="/favorites">
+        <FavoritesPage/>
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 // Render our custom HTML element into the place of the 'root' element
-ReactDOM.render( <App />, document.getElementById('root') );
+ReactDOM.render( <BrowserRouter><App /></BrowserRouter> , document.getElementById('root') );
 
 
